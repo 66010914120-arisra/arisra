692ad8d36d7d3030022c3188361587009bd9d486
66010914120-arisra <66010914120@msu.ac.th>
2026-02-10 14:34:29 +0700

    a

--- i/b.php
+++ i/b.php
@@ -1,57 +1,48 @@
-<?php include_once("connectdb.php"); ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="th">
 <head>
 <meta charset="utf-8">
 <title>ข้อมูลจังหวัด</title>
-</head>
-<body>
 
-<h1>งาน i</h1>
-<h2>ข้อมูลจังหวัด --- อริศรา พวงมาลัย (กุ๊ก)</h2>
-
-<form method="post">
-    ชื่อจังหวัด
-    <input type="text" name="pname" required><br>
-
-    ชื่อภาค
-    <select name="rid">
-        <?php
-        $sql3 = "SELECT * FROM regions ORDER BY r_name ASC";
-        $rs3  = mysqli_query($conn, $sql3);
-        while ($r = mysqli_fetch_assoc($rs3)) {
-        ?>
-            <option value="<?php echo $r['r_id']; ?>">
-                <?php echo $r['r_name']; ?>
-            </option>
-        <?php } ?>
-    </select><br><br>
-
-    <button type="submit" name="Submit">บันทึก</button>
-</form>
-
-<br><br>
-
-<?php
-/* ===== บันทึกข้อมูล ===== */
-if (isset($_POST['Submit'])) {
-    $pname = $_POST['pname'];
-    $rid   = $_POST['rid'];
-
-    // กำหนดนามสกุลเป็น jpg
-    $sql = "INSERT INTO provinces (p_name, p_ext, r_id)
-            VALUES ('$pname','jpg','$rid')";
-    mysqli_query($conn, $sql);
+<style>
+body{
+    font-family: Tahoma, sans-serif;
 }
 
-/* ===== แสดงข้อมูล ===== */
-$sql = "SELECT p.*, r.r_name
-        FROM provinces p
-        INNER JOIN regions r ON p.r_id = r.r_id";
-$rs = mysqli_query($conn, $sql);
-?>
+table{
+    border-collapse: collapse;
+    width: 90%;
+    margin: auto;
+}
 
-<table border="1" cellpadding="5">
+th, td{
+    border: 1px solid #000;
+    padding: 6px;
+    text-align: center;
+    vertical-align: middle;
+}
+
+th{
+    background: #f2f2f2;
+}
+
+td.name{
+    text-align: left;
+    padding-left: 10px;
+}
+
+img{
+    width: 120px;   
+    height: auto;
+}
+</style>
+</head>
+
+<body>
+
+<h2>ข้อมูลจังหวัด --- อริศรา พวงมาลัย (กุ๊ก)</h2>
+
+<table>
 <tr>
     <th>รหัส</th>
     <th>ชื่อจังหวัด</th>
@@ -59,14 +50,25 @@ $rs = mysqli_query($conn, $sql);
     <th>ภาค</th>
 </tr>
 
-<?php while ($data = mysqli_fetch_assoc($rs)) { ?>
+<?php
+include_once("connectdb.php");
+
+$sql = "SELECT p.p_id, p.p_name, p.p_ext, r.r_name
+        FROM provinces p
+        INNER JOIN regions r
+        ON p.r_id = r.r_id
+        ORDER BY p.p_id ASC";
+
+$rs = mysqli_query($conn, $sql);
+
+while($data = mysqli_fetch_array($rs)){
+?>
 <tr>
     <td><?php echo $data['p_id']; ?></td>
-    <td><?php echo $data['p_name']; ?></td>
-<td>
-    <img src="img/<?php echo $data['p_id']; ?>.jpg" width="120">
-</td>
-
+    <td class="name"><?php echo $data['p_name']; ?></td>
+    <td>
+        <img src="images/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>">
+    </td>
     <td><?php echo $data['r_name']; ?></td>
 </tr>
 <?php } ?>
