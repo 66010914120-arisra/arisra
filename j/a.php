70e5d4f36061dee67ede3320ac49023384656718
arisra <66010914120@msu.ac.th>
2026-02-17 11:46:35 +0700

    a

--- j/a.php
+++ j/a.php
@@ -42,7 +42,7 @@ $rs  = mysqli_query($conn, $sql);
     <tr>
         <td><?php echo $data['r_id']; ?></td>
         <td><?php echo $data['r_name']; ?></td>
-        <td widht="50"align="center"><a href="delete_region.php?id=<?php echo $data['r_id']; ?>" onClick="return confirm('ยืนยันการลบ?');"><img src="3.png"width="20"></a></td>
+        <td widht="50"align="center"><a href="delete_region.php?id=<?php echo $data['r_id']; ?>" onClick="return confirm('ยืนยันการลบ?');"><img src="3.jpg"width="20"></a></td>
     </tr>
     <?php } ?>
 </table>
